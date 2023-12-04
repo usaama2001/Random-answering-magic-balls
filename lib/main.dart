e99267ecd7f4f6fe808d206b01f56f59f25f388e
usaama2001 <muxudiinusaama@gmail.com>
2023-12-04 00:22:10 -0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text(
+          'Ask Me Anything',
+          style: TextStyle(fontSize: 32.0),
+        ),
+        backgroundColor: Colors.indigo[600],
+      ),
+      body: Questions(),
+    ),
+  ));
+}
+
+class Questions extends StatefulWidget {
+  const Questions({super.key});
+  @override
+  State<Questions> createState() => _QuestionsState();
+}
+
+class _QuestionsState extends State<Questions> {
+  int answer = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            answer = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$answer.png'),
+      ),
+    );
+  }
+}
